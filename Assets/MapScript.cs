824189d439e9a33088210a90ddb01246d3b23cf4
Mitamura <mitamura@rrj.jp>
2019-04-26 21:25:04 +0900

    first commit

---
+++ Assets/MapScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapScript : MonoBehaviour
+{
+
+    public GameObject treePrefab;
+    public GameObject movingObj;
+
+    void Start()
+    {
+        StartCoroutine(MoveMap());
+        StartCoroutine(SetTree());
+    }
+
+    IEnumerator SetTree()
+    {
+        while (true)
+        {
+            Vector3 pos = new Vector3(11, 3.6f, 0);
+            GameObject tree = Instantiate(treePrefab, pos, transform.rotation) as GameObject;
+            tree.transform.parent = movingObj.transform;
+            yield return new WaitForSeconds(0.7f);
+        }
+    }
+
+    IEnumerator MoveMap()
+    {
+        while (true)
+        {
+            Vector3 pos = movingObj.transform.position;
+            pos.x -= 1 * Time.deltaTime;
+            movingObj.transform.position = pos;
+            yield return 0;
+        }
+    }
+
+}
---
+++ Assets/resetScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class resetScript : MonoBehaviour
+{
+    public void LoadMain()
+    {
+        Application.LoadLevel("SampleScene");
+    }
+}
